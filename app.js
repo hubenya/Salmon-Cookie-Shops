806edc6ae92e9f7339dfe4eb7750ce9aac718bb6
Slavik <hubenya@hotmail.com>
2017-05-15 14:42:23 -0700

    added new details to HTML and app

---
+++ app.js
@@ -0,0 +1,84 @@
+var firstAndPike = {
+  location: 'First and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  details: function() {
+    return 'Salmon Cookie shop at ' + this.location[0] + ' minimum of ' + this.minCust[0] + ' customers and maximum of ' + this.minCust[0] + '. Whihc averages out to ' + this.avgCookieSale;
+  }
+};
+
+firstAndPike.details();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var seaTacAirport = {
+  location: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+  details: function() {
+    return 'Salmon Cookie shop at ' + this.location + ' has minimum of ' + this.minCust + ' customers and maximum of ' + this.minCust + '. Which averages out to ' + this.avgCookieSale;
+  }
+};
+
+seaTacAirport.details();
+
+var seattleCenter = {
+  location: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
+  details: function() {
+    return 'Salmon Cookie shop at ' + this.location + ' has minimum of ' + this.minCust + ' customers and maximum of ' + this.minCust + '. Which averages out to ' + this.avgCookieSale;
+  }
+};
+
+seattleCenter.details();
+
+var capitolHill = {
+  location: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+  details: function() {
+    return 'Salmon Cookie shop at ' + this.location + ' has minimum of ' + this.minCust + ' customers and maximum of ' + this.minCust + '. Which averages out to ' + this.avgCookieSale;
+  }
+};
+
+capitolHill.details();
+
+var Alki = {
+  location: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+  details: function() {
+    return 'Salmon Cookie shop at ' + this.location + ' has minimum of ' + this.minCust + ' customers and maximum of ' + this.minCust + '. Which averages out to ' + this.avgCookieSale;
+  }
+};
+
+Alki.details();
