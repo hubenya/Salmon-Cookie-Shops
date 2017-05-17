083b9b64882eba10a3c18d98eef661536eac1bd8
hubenya <hubenya@hotmail.com>
2017-05-16 21:14:21 -0700

    was not successfull to add total to the printed list

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 //I wanted to practice and make it loop but was not able to loop more than once. i feel like i'm missing something
 var hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm',];
 // hours.length
@@ -9,7 +10,8 @@ function Store(location, minimum, maximum, averageSales) {
   this.maximum = maximum;
   this.averageSales = averageSales;
   this.cookiesSoldPerHour = [];
-  this.totalSoldCookies = 0;
+  this.totalCookiesLocation = 0;
+
 }
 //random customer genrator that come through the door
 Store.prototype.randomNumberGenarator = function(){
@@ -20,16 +22,21 @@ Store.prototype.randomNumberGenarator = function(){
 Store.prototype.cookieSoldEachHour = function() {
   for (var i = 0; i < hours.length; i++) {
     this.cookiesSoldPerHour.push(Math.floor(this.averageSales * this.randomNumberGenarator()));
+     //this.totalCookiesLocation += this.cookiesSoldPerHour[i];
   }
 
+}
   //console.log(this.cookieSoldEachHour());
   //I was testing my code to see where my problem is.
   //console.log('what cookies sold per hour', this.cookiesSoldPerHour);
-};
-function sumArray(this.cookiesSoldPerHour) {
-  var total = this.cookiesSoldPerHour + this.cookieSoldEachHour();
-  return total;
+   this.totalCookiesDaily = function() {
+    for (var j = 0; j < this.cookiesSoldPerHour; j++) {
+     this.totalCookiesLocation += this.cookiesSoldPerHour[j];
+
+  //console.log('Total cookies daily' + totalCookiesDaily());
 }
+
+};
 
 function storeList(store) {
   var container = document.createElement('div');
@@ -63,9 +70,10 @@ SeaTacAirport.cookieSoldEachHour();
 SeattleCenter.cookieSoldEachHour();
 CapitolHill.cookieSoldEachHour();
 Alki.cookieSoldEachHour();
+//totalCookiesDaily(FirstandPike);
 storeList(FirstandPike);
 storeList(SeaTacAirport);
 storeList(SeattleCenter);
 storeList(CapitolHill);
 storeList(Alki);
-totalLocation();
+//this.totalCookiesDaily(FirstandPike);
