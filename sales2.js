29128fdd9ef86c6b7cfeff25e0f01ba1ba1f6741
hubenya <hubenya@hotmail.com>
2017-06-27 23:55:19 -0700

    added susen age

--- sales2.js
+++ sales2.js
@@ -86,3 +86,21 @@ bob.setAge = setAge;
 
 // change bob's age to 50 here
 bob.setAge(50);
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+
+// make susan here, and first give her an age of 25
+var susan = new Object();
+susan.age = 25;
+susan.setAge = setAge;
+
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35);
