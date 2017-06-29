d26725ce2f1f21fbe45b9a93982fccf8cb08dd5e
hubenya <hubenya@hotmail.com>
2017-06-28 23:01:15 -0700

    added new rabits

--- sales2.js
+++ sales2.js
@@ -166,3 +166,53 @@ var george = new Person("George Washington", 275);
 
 
 console.log('We Know about', george);
+
+//calling new person
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39);
+var holden = new Person("Holden Caulfield", 16);
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " + holden.age);
+
+//calcPerimeter function
+
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+    this.calcPerimeter = function() {
+      return (this.height + this.width) * 2;
+};
+}
+
+var rex = new Rectangle(7,3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
+
+
+//Rabit function with adjective
+
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();
