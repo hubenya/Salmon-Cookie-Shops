d9f2156291a2d85332a93c7e6078ddb7451fd1d0
hubenya <hubenya@hotmail.com>
2017-06-28 22:31:57 -0700

    added area and perimeter

---
+++ sales2.js
@@ -0,0 +1,142 @@
+// Our bob object again, but made using a constructor this time
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// Here is susan1, in literal notation
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
+};
+// Make a new susan2 object, using a constructor instead
+
+
+
+
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = {
+    species: "beagle",
+    age: 10
+};
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+
+
+var buddy = {};
+buddy.species = "golden retriever";
+buddy.age = 5;
+
+
+
+
+
+var bicycle = {
+    speed: 0,
+    gear: 1,
+    frame_material: "carbon fiber"
+};
+
+
+var multiply = function (x, y) {
+  return x * y;
+};
+
+
+multiply(Math.random(), 200);
+//console.log("console loggggiiinnn", Math.random());
+
+
+// here is bob again, with his usual properties
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+// here we set bob's age to 40
+bob.setAge(20);
+// bob's feeling old.  Use our method to set bob's age to 20
+
+
+
+
+var bob = new Object();
+bob.age = 17;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+
+bob.getYearOfBirth = function () {
+  return 2014 - bob.age;
+};
+console.log(bob.getYearOfBirth());
+
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+// and down here we just use the method we already made
+bob.setAge = setAge;
+
+// change bob's age to 50 here
+bob.setAge(50);
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+
+// make susan here, and first give her an age of 25
+var susan = new Object();
+susan.age = 25;
+susan.setAge = setAge;
+
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35);
+
+
+//setting heigth and width for ractangle
+
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+};
+// help by finishing this method
+rectangle.setWidth = function (newWidth) {
+    this.width = newWidth;
+};
+
+// here change the width to 8 and height to 6 using our new methods
+rectangle.setWidth(8);
+rectangle.setHeight(6);
+
+//perimeter and area methods
+
+var square = new Object();
+square.sideLength = 6;
+square.calcPerimeter = function() {
+  return this.sideLength * 4;
+};
+// help us define an area method here
+square.calcArea = function() {
+  return this.sideLength * this.sideLength;
+};
+
+var p = square.calcPerimeter();
+var a = square.calcArea();
+console.log('Area is', square.calcArea());
+console.log('Perimeter is', square.calcPerimeter());
